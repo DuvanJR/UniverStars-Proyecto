94aca09584f2705cac3007a33180e628657372c8
DuvanJR <duvanjaimes14@gmail.com>
2023-10-31 22:44:49 -0500

    Cambios en el contenedor de imagenes, agrtegacion de imagenes estaticas

--- src/app/ContenedorComponent/ContenedorComponent.component.ts
+++ src/app/ContenedorComponent/ContenedorComponent.component.ts
@@ -17,14 +17,14 @@ export class ContenedorComponentComponent {
   }
 
   informacion  = [
-    { titulo: 'Universidad de Cartagena', contenido: 'Este es el contenido del contenedor 1.', ciudad: 'cartagena' },
-    { titulo: 'UTB', contenido: 'Este es el contenido del contenedor 2.', ciudad: 'cartagena'},
-    { titulo: 'Tecnologico Comfenalco', contenido: 'Este es el contenido del contenedor 3.', ciudad: 'cartagena' },
-    { titulo: 'Universidad de San Buenaventura', contenido: 'Este es el contenido del contenedor 4.', ciudad: 'cartagena' },
-    { titulo: 'Universidad del atlantico', contenido: 'Este es el contenido del contenedor 5.', ciudad: 'barranquilla' },
-    { titulo: 'Universidad de Antioquia', contenido: 'Este es el contenido del contenedor 6.', ciudad: 'medellin' },
-    { titulo: 'Universidad Nacional', contenido: 'Este es el contenido del contenedor 7.' , ciudad: 'bogota'},
-    { titulo: 'Universidad de Cali', contenido: 'Este es el contenido del contenedor 8.', ciudad: 'cali'},
+    { titulo: 'Universidad de Cartagena', contenido: 'Este es el contenido del contenedor 1.', ciudad: 'cartagena', src: '../../assets/img/Universidad Cartagena.jpg'},
+    { titulo: 'UTB', contenido: 'Este es el contenido del contenedor 2.', ciudad: 'cartagena', src: '../../assets/img/Universidad UTB.jpeg' },
+    { titulo: 'Tecnologico Comfenalco', contenido: 'Este es el contenido del contenedor 3.', ciudad: 'cartagena', src: '../../assets/img/Tecno Comfenalco.jpeg'},
+    { titulo: 'Universidad de San Buenaventura', contenido: 'Este es el contenido del contenedor 4.', ciudad: 'cartagena', src: '../../assets/img/Universidad SanBue.jpg' },
+    { titulo: 'Universidad del atlantico', contenido: 'Este es el contenido del contenedor 5.', ciudad: 'barranquilla', src: '../../assets/img/Universidad Atlantico.jpeg' },
+    { titulo: 'Universidad de Antioquia', contenido: 'Este es el contenido del contenedor 6.', ciudad: 'medellin', src: '../../assets/img/Universidad Antioquia.jpg' },
+    { titulo: 'Universidad Nacional', contenido: 'Este es el contenido del contenedor 7.' , ciudad: 'bogota', src: '../../assets/img/Universidad Nacional.jpg'},
+    { titulo: 'Universidad de Cali', contenido: 'Este es el contenido del contenedor 8.', ciudad: 'cali' , src: '../../assets/img/Universidad Cali.jpg'},
     { titulo: 'Universidad de Bucaramanga', contenido: 'Este es el contenido del contenedor 9.' , ciudad: 'bucaramanga'},
     { titulo: 'Universidad de Boyaca', contenido: 'Este es el contenido del contenedor 10.', ciudad: 'boyaca'},
     // ... Más objetos de datos
